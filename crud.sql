db80208e6a8c607db60edbd537bbca1cee72c436
Eda <edda.yavuz@gmail.com>
2024-09-29 13:17:43 +0300

    Update crud.sql
    added  year function

--- crud.sql
+++ crud.sql
@@ -6,6 +6,10 @@ MODIFY DateOfBirth__1 datetime;
 INSERT INTO company.csvjson (UserID, FirstName, LastName, Sex, Phone, DateOfBirth__1, JobTitle)
 VALUES ('AB12345678', 'Eda', 'Yavuz', 'Female', '123-456-7890', '1993-08-25', 'developer');
 
+/* YEAR Function */
+SELECT * FROM company.csvjson
+WHERE YEAR(DateOfBirth__1) > 2010;
+
 /* CALCULATING THE AGE FROM DATE TIME COLUMN */
 SELECT FirstName, FLOOR(DATEDIFF(NOW(), DateOfBirth__1)/ 365) as age FROM company.csvjson;
 
