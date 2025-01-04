ce1cab01fd390d9189ccdc8d03760e7e3a38c88a
Eda <edda.yavuz@gmail.com>
2025-01-05 00:50:16 +0300

    Update loginForm.java

--- GUI/loginForm.java
+++ GUI/loginForm.java
@@ -2,13 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class loginForm extends JFrame{
+public class events extends JFrame{
     private JPanel panel;
     private JButton loginButton;
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JLabel warning;
     private JLabel pic;
+    private JCheckBox iAgreeToTermsCheckBox;
 
 
     public events() {
@@ -17,12 +18,14 @@ public class loginForm extends JFrame{
         setSize(500,500);
         setContentPane(panel);
         setVisible(true);
+        loginButton.setEnabled(false);
 
         textField1.addFocusListener(new FocusAdapter() {
             @Override
             public void focusLost(FocusEvent e) {
                 super.focusLost(e);
                 if (textField1.getText().length() <= 5){
+                    warning.setForeground(Color.RED);
                     warning.setText("username can't be smaller than 5 characters.");
                   textField1.requestFocus();
                 }else{
@@ -42,6 +45,13 @@ public class loginForm extends JFrame{
                 }
             }
         });
+        iAgreeToTermsCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                loginButton.setEnabled(iAgreeToTermsCheckBox.isSelected());
+            }
+        });
     }
 
 }
+
